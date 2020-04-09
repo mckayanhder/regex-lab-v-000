2c3e153a2bdf525c2303b8639d8f9071c68b0be9
McKay Anhder <anhder.mckay@gmail.com>
2020-04-09 05:58:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 
 def starts_with_a_vowel?(word)
-  if word == word.match(/\A[aeiou]/)
+  if word == word.match?(/\A[aeiou]/)
     true
   elsif
     false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
